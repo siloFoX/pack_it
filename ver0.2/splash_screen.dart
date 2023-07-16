de6e74e9d0695cebd73c5fcea07d91a8251e3b5c
silofox <ladnav0704@gmail.com>
2023-07-16 03:43:57 +0000

    (ver0.2) Finish splash screen

--- ver0.2/splash_screen.dart
+++ ver0.2/splash_screen.dart
@@ -4,73 +4,71 @@ void main() {
   runApp(SplashScreen());
 }
 
-final splashScreenTheme  = ThemeData(
-  scaffoldBackgroundColor : Colors.white,
-  textTheme : const TextTheme(
-    bodyText1 : TextStyle(
-      color : Colors.black,
-      fontSize : 10.0,
-      fontWeight : FontWeight.w600,
-    ),
-    bodyText2 : TextStyle(
-      color : Colors.black,
-      fontSize : 10.0,
-      fontWeight : FontWeight.w400,
-    ),
-  ),
-);
-
-extension CustomColor on ThemeData {
-  Color get malachite => const Color(0xff00cb08);
-  Color get mustard => const Color(0xfffed058);
-}
-
-extension TitleCustomTheme on TextTheme {
-  TextStyle get title => const TextStyle(
+// splash screen theme
+class SplashScreenTheme {
+  static const Color primaryColorMalachite = Color(0xff00cb08);
+  static const Color secondaryColorMustard = Color(0xfffed058);
+  
+  static const TextStyle bodyText1 = TextStyle(
+    color : Colors.black,
+    fontSize : 15.0,
+    fontWeight : FontWeight.w600,
+  );
+  static const TextStyle bodyText2 = TextStyle(
+    color : Colors.black,
+    fontSize : 15.0,
+    fontWeight : FontWeight.w400,
+  );
+  static const TextStyle titleText = TextStyle(
     color : Colors.black,
     fontSize : 20.0,
     fontWeight : FontWeight.w800,
   );
+  static const double underlineWidth = 30;
 }
 
+// splash screen
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner : false,
-      theme : splashScreenTheme,
       home : Scaffold(
         body : Center(
           child : Column(
             mainAxisAlignment : MainAxisAlignment.center,
             children : [
+
+              // first line
               Row(
                 mainAxisAlignment : MainAxisAlignment.center,
                 children : [
                   Stack(
                     children : [
-                      Container(
-                        width : 25,
-                        height : 17,
-                        child : Column(
-                          mainAxisAlignment : MainAxisAlignment.end,
-                          children : [
-                            Container(
-                              height : 7,
-                              color : Theme.of(context).mustard,
-                            ),
-                          ],
-                        ),
-                      ),
                       Row(
                         children : [
+                          const SizedBox(width : 2.5),
+                          Column( 
+                            children : [
+                              const SizedBox(width : SplashScreenTheme.underlineWidth, height : 10),
+                              Container(
+                                width : SplashScreenTheme.underlineWidth,
+                                height : 7,
+                                color : SplashScreenTheme.secondaryColorMustard,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const Row(
+                        children : [
                           Text(
-                            "여행",
-                            style : Theme.of(context).textTheme.bodyText1,
+                            " 여행",
+                            style : SplashScreenTheme.bodyText1,
                           ),
                           Text(
                             " 갈 때,",
-                            style : Theme.of(context).textTheme.bodyText2,
+                            style : SplashScreenTheme.bodyText2,
                           ),
                         ],
                       ),
@@ -78,9 +76,11 @@ class SplashScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              Text(
+
+              // second line
+              const Text(
                 "쉽고 빠른 짐싸기",
-                style : Theme.of(context).textTheme.bodyText2,
+                style : SplashScreenTheme.bodyText2,
               ),
               const SizedBox(height : 8.0),
               Container(
@@ -89,14 +89,18 @@ class SplashScreen extends StatelessWidget {
                 color : Colors.black,
               ),
               const SizedBox(height : 8.0),
-              Icon(
+
+              // logo icon
+              const Icon(
                 Icons.lock_outline,
                 size : 60.0,
-                color : Theme.of(context).malachite,
+                color : SplashScreenTheme.primaryColorMalachite,
               ),
-              Text(
+
+              // logo
+              const Text(
                 "짐고",
-                style : Theme.of(context).textTheme.title,
+                style : SplashScreenTheme.titleText,
               ),
             ],
           ),
